43bc395b1ea07fe25e879474bde29488425f59d3
Kento Nishi <kento24gs@outlook.com>
2021-07-24 22:58:15 -0700

    materialify is a dependency

--- ui-components/utils/package.js
+++ ui-components/utils/package.js
@@ -2,25 +2,7 @@
 
 const fs = require('fs');
 
-let defaultPackageJson = {
-  "name": "@livetl/ui-components",
-  "version": "0.0.1",
-  "description": "LiveTL's UI components",
-  "license": "AGPL-3.0",
-  "author": "LiveTL Developers",
-  "repository": {
-    "type": "git",
-    "url": "https://github.com/LiveTL/Common.git",
-    "directory": "javascript/ui-components"
-  },
-  "bugs": {
-    "url": "https://github.com/LiveTL/Common/issues"
-  },
-  "homepage": "https://github.com/LiveTL/Common#readme",
-  "funding": {
-    "url": "https://opencollective.com/livetl"
-  }
-};
+let defaultPackageJson = require('./package.json');
 
 let ltlPackageJson = require('../submodules/LiveTL/package.json');
 let deps = ltlPackageJson.dependencies;
