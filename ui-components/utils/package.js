f409848fc27c6ee414d26a70a33a8d4dec670ffc
Kento Nishi <kento24gs@outlook.com>
2021-07-24 22:32:08 -0700

    trial action, see wut happens lol

---
+++ ui-components/utils/package.js
@@ -0,0 +1,39 @@
+// create package.json
+
+const fs = require('fs');
+
+let defaultPackageJson = {
+  "name": "@livetl/ui-components",
+  "version": "0.0.1",
+  "description": "LiveTL's UI components",
+  "license": "AGPL-3.0",
+  "author": "LiveTL Developers",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/LiveTL/Common.git",
+    "directory": "javascript/ui-components"
+  },
+  "bugs": {
+    "url": "https://github.com/LiveTL/Common/issues"
+  },
+  "homepage": "https://github.com/LiveTL/Common#readme",
+  "funding": {
+    "url": "https://opencollective.com/livetl"
+  }
+};
+
+let ltlPackageJson = require('../submodules/LiveTL/package.json');
+let deps = ltlPackageJson.dependencies;
+Object.keys(deps).forEach(dep => {
+  if (dep.includes('vue')) delete deps[dep];
+});
+
+const finalPackageJson = Object.assign(defaultPackageJson, {
+  dependencies: deps
+});
+fs.writeFileSync('./package.json', JSON.stringify(
+  finalPackageJson,
+  '',
+  2
+), 'utf8');
+
