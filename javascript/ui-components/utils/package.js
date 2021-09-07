2b2054f548c2705639f5c613ec563d28eed213b9
Kento Nishi <kento24gs@outlook.com>
2021-09-07 12:31:39 -0700

    [ui] release v0.0.10

--- javascript/ui-components/utils/package.js
+++ javascript/ui-components/utils/package.js
@@ -19,3 +19,8 @@ fs.writeFileSync('./build/package.json', JSON.stringify(
   2
 ), 'utf8');
 
+fs.writeFileSync('./build/package-livetl.json', JSON.stringify({
+  description: ltlPackageJson.description,
+  version: ltlPackageJson.version,
+  ...JSON.parse(JSON.stringify(require("../submodules/LiveTL/src/manifest.json")))
+});
